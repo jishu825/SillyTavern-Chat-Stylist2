e588513db80bc5fdc3cd9542afaff560a7d3912d
jishu825 <2805672171@qq.com>
2024-12-02 21:37:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,22 +16,28 @@ window['extensions'][MODULE_NAME] = {
 
 function init() {
     // Create the activation button
-    const settingsHtml = `
-        <div class="chat-stylist-settings">
-            <div class="inline-drawer">
-                <div class="inline-drawer-toggle inline-drawer-header">
-                    <b>Chat Stylist</b>
-                    <div class="inline-drawer-icon fa-solid fa-circle-chevron-down down"></div>
-                </div>
-                <div class="inline-drawer-content">
-                    <div class="chat-stylist-button menu_button" id="chat-stylist-button">
+    // Updated button HTML in init function
+const settingsHtml = `
+    <div class="chat-stylist-settings">
+        <div class="inline-drawer">
+            <div class="inline-drawer-toggle inline-drawer-header">
+                <b>Chat Stylist / 聊天样式编辑器</b>
+                <div class="inline-drawer-icon fa-solid fa-circle-chevron-down down"></div>
+            </div>
+            <div class="inline-drawer-content">
+                <div class="chat-stylist-button menu_button" id="chat-stylist-button">
+                    <div class="button_content">
                         <i class="fa-solid fa-palette"></i>
-                        Open Style Editor
+                        <span class="button_text">
+                            <span class="button_text_en">Style Editor</span>
+                            <span class="button_text_zh">样式编辑器</span>
+                        </span>
                     </div>
                 </div>
             </div>
         </div>
-    `;
+    </div>
+`;
 
     $('#extensions_settings2').append(settingsHtml);
 
